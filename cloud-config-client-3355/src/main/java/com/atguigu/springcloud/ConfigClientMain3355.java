8286b575bb9bef54ca6573fd18c9b2bd61055540
xcxchzp <454241171@qq.com>
2020-12-25 16:56:49 +0800

    消息总线进度提交

---
+++ cloud-config-client-3355/src/main/java/com/atguigu/springcloud/ConfigClientMain3355.java
@@ -0,0 +1,16 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Auther: hzp
+ * @Date: 2020/12/24 17:12
+ * @Description:
+ */
+@SpringBootApplication
+public class ConfigClientMain3355 {
+    public static void main(String[] args) {
+        SpringApplication.run( ConfigClientMain3355.class,args);
+    }
+}
